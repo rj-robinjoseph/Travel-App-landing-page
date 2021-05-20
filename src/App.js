16fd61f6a3be4b4f9e4a05e6a0d33fa1a89b855c
Robin Joseph <robinjoseph452@gmail.com>
2021-05-20 11:55:41 +0530

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png'
+import menu from './menu.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+            <div className="navbar">
+                <img src={logo} alt="" className="logo" />
+                <nav>
+                    <ul>
+                        <li><a href="##">HOME</a></li>
+                        <li><a href="##">REGION</a></li>
+                        <li><a href="##">ABOUT</a></li>
+                    </ul>
+                </nav>
+                <img src={menu} alt="" className="menu-icon" />
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <h1>Sahara</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, iste odit tempora laboriosam magni assumenda blanditiis laudantium voluptatum modi doloremque doloribus quisquam veritatis possimus fugit expedita? Id fuga molestias quae!</p>
+                <button type="button">Explore</button>
+                </div>
+                <div className="col">
+                    <div className="card card1">
+                        <h5>Western Desert</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div className="card card2">
+                        <h5>AI Bagawat</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div className="card card3">
+                        <h5>Pyramid of Giza</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div className="card card4">
+                        <h5>Kalahari Desert</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
     </div>
   );
 }
